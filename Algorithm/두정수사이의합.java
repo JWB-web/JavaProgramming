20b45234ee2dec339623fb19848a40d87162ae7c
WooBinJang <59834529+WooBinJang@users.noreply.github.com>
2020-08-19 22:19:39 +0900

    Add files via upload

---
+++ Algorithm/두정수사이의합.java
@@ -0,0 +1,17 @@
+package Algorithm;
+
+class 두정수사이의합 {
+	public long solution(int a, int b) {
+		long answer = 0;
+		if (a <= b) {
+			for (int i = a; i <= b; i++) {
+				answer += i;
+			}
+		} else if (a > b) {
+			for (int i = b; i <= a; i++) {
+				answer += i;
+			}
+		}
+		return answer;
+	}
+}
