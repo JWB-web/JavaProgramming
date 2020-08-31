b91b63ce70c422cb31fcabca9faa64e09754f848
WooBinJang <59834529+WooBinJang@users.noreply.github.com>
2020-08-31 22:59:48 +0900

    Create 완주하지_못한_선수.java

---
+++ 완주하지_못한_선수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+  	public static String solution(String[] participant, String[] completion) {
+		String answer = "";
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+			for (int i = 0; i < completion.length; i++) {
+				if(!completion[i].equals(participant[i])) {
+					answer = participant[i];
+                    break;
+				} else if (completion[completion.length-1].equals(participant[completion.length-1])) {
+					answer = participant[completion.length];
+                  
+				}
+		}
+
+		return answer;
+	}
+  }
